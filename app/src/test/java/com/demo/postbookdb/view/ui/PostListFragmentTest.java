67fcd70f6cdaa99ad1974fa849b48caaf69c3f2a
@m's <amin.manjiyani57@gmail.com>
2019-12-06 14:15:00 +0530

    anroid test case files

---
+++ app/src/test/java/com/demo/postbookdb/view/ui/PostListFragmentTest.java
@@ -0,0 +1,38 @@
+package com.demo.postbookdb.view.ui;
+
+import androidx.fragment.app.Fragment;
+
+import com.demo.postbookdb.service.model.UserPost;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PostListFragmentTest {
+
+    @Mock
+    UserPost userPost;
+
+    @Test
+    public void onClickTest() {
+    }
+
+
+    @Test
+    public void onFavClick() {
+    }
+
+    @Test
+    public void onFavBtnClick() {
+    }
+
+    @Test
+    public void onAllBtnClick() {
+    }
+}
