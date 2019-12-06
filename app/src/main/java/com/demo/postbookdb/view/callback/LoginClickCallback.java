00077dd87853e7cbb94f898292c2c989ed0848fb
@m's <amin.manjiyani57@gmail.com>
2019-12-06 14:07:03 +0530

    it will handle login user event and generate inteface file so it can be implemted in hamdler

---
+++ app/src/main/java/com/demo/postbookdb/view/callback/LoginClickCallback.java
@@ -0,0 +1,9 @@
+package com.demo.postbookdb.view.callback;
+
+
+import com.demo.postbookdb.service.model.UserPost;
+
+/*callback from login XML*/
+public interface LoginClickCallback {
+    void onClick(UserPost userPost);
+}
